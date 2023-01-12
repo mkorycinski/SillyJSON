22fb41db86dcfe7086c3902e31c7cba0508cb527
mateuszko <mateusz.korycinski@nask.pl>
2023-01-12 14:10:28 +0100

    a bit more informative error

--- silly_json/errors.py
+++ silly_json/errors.py
@@ -1,3 +1,11 @@
 class NotExpectedType(Exception):
-    def __init__(self, position=None):
+    def __init__(self, value: str, position=None):
         self.position = position
+        self.value = value
+        self.msg = f'Not expected value: {self.value} encountered. JSON is malformed.'
+
+    def __str__(self):
+        return self.msg
+
+    def __repr__(self):
+        return f'<NotExpectedType: {self.value}>'
