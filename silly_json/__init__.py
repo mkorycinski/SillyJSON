b83184471ed07dcc0053799fbe8e5e3d09b05f7e
mateuszko <mateusz.korycinski@nask.pl>
2023-01-12 14:22:51 +0100

    docstrings in init

--- silly_json/__init__.py
+++ silly_json/__init__.py
@@ -2,11 +2,27 @@ from .json_parser import JSONParser
 
 
 def load(fpath: str) -> object:
+    """
+    Parse a JSON file.
+    Args:
+        fpath: A path to a JSON file.
+
+    Returns:
+        Parsed json as an object.
+    """
     parser = JSONParser()
     with open(fpath, 'r') as jfile:
         return parser(jfile.read())
 
 
-def loads(x: str) -> object:
+def loads(s: str) -> object:
+    """
+    Parses a JSON-formatted string.
+    Args:
+        s: JSON-formatted string.
+
+    Returns:
+        Parsed json as an object.
+    """
     parser = JSONParser()
-    return parser(x)
+    return parser(s)
