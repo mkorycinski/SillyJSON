760d0b506aebcdacf46a14fc6f67618285b14924
mateuszko <mateusz.korycinski@nask.pl>
2023-01-12 11:54:51 +0100

    tests for object splitter

---
+++ tests/test_object_splitter.py
@@ -0,0 +1,27 @@
+import unittest
+
+from unittest_expander import expand, foreach
+
+from silly_json.utils import object_splitter
+
+@expand
+class TestObjectSplitter(unittest.TestCase):
+
+    @foreach(
+        (
+            '[1, 0.001, true, false, null, "ja,cek", "jacek"]',
+            ['1', '0.001', 'true', 'false', 'null', '"ja,cek"', '"jacek"']
+        ),
+        (
+            '[1, 0.001, true, false, null, "ja,cek", "jacek", {"foo: "bar"}, {"foo: "bar", ' \
+            '"foo: "b,ar"}]',
+            ['1', '0.001', 'true', 'false', 'null', '"ja,cek"', '"jacek"', '{"foo: "bar"}',
+             '{"foo: "bar","foo: "b,ar"}']
+        ),
+        (
+            '{"foo: "bar", "foo: "b,ar"}',
+            ['"foo: "bar"', '"foo: "b,ar"']
+        )
+    )
+    def test_object_splitter(self, input_str, expected):
+        self.assertListEqual(object_splitter(input_str=input_str), expected)
